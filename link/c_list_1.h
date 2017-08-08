761e3acf7ce2f6d689ad222a2b9da337273f9504
magox <46257902@qq.com>
2017-08-08 15:14:45 +0800

    添加方法

--- link/c_list_1.h
+++ link/c_list_1.h
@@ -18,3 +18,24 @@ typedef struct cList
 	uint32_t size;
 }
 
+//方法
+
+/* 新建一个节点 */
+cListNode*  c_list_node_create( int size );
+
+/* 初始化一个双向链表 */
+cList* c_list_init( int size );
+
+/* 册除一个链表 */
+void c_list_free( cList *list );
+
+/* 在当前节点后添加一个节点 */
+void _c_list_push_back( cList *list, cListNode *nd );
+
+/* 在当前节点前添加一个节点 */
+void _c_list_push_front( cList *list, cListNode *nd );
+
+/* 在当前节点后添加一个节点的内容 */
+void c_list_push_back( cList *list, void *data, int size );
+/* 在当前节点前添加一个节点的内容 */
+void _c_list_push_front( cList *list, void *data, int size );
