716ecc6e05eefcc61ec9d8da4138e9086192ed05
magox <46257902@qq.com>
2017-08-09 15:59:50 +0800

    update

---
+++ IO/select_test.c
@@ -0,0 +1,91 @@
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <assert.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <errno.h>
+#include <string.h>
+#include <fcntl.h>
+#include <stdlib.h>
+
+int  main ( int argc, char* argv[] )
+{
+	if( argc <=2 )
+	{
+		printf("usage: %s ip_address port_number\n", basename( argv[0] ) );
+		return 1;
+	}
+
+	const char* ip = argv[1];
+	int port = atoi( argv[2] );
+
+	int ret = 0;
+	struct sockaddr_in address;
+	bzero( &address, sizeof( address ) );
+	address.sin_family = AF_INET;
+	inet_pton( AF_INET, ip, &address.sin_addr );
+	address.sin_port = htons( port );
+
+	int listenfd = socket( PF_INET, SOCK_STREAM, 0 );
+	assert( listenfd >= 0 );
+	ret = bind( listenfd, ( struct sockaddr* )&address, sizeof( address ) );
+	assert( ret != -1 );
+	ret = listen( listenfd, 5 );
+
+	struct sockaddr_in client_address;
+	socklen_t client_addrlength = sizeof( clent_address );
+
+	int connfd = accept( listenfd, ( struct sockaddr* )&client_address, &client_addrlength );
+
+	if( connfd < 0  )
+	{
+		printf("errno is %d\n",errno );
+		close( listenfd );
+	}
+
+	char_buf[1024];
+	fd_set read_fds;
+	fd_set exception_fds;
+	FD_ZERO( &read_fds );
+	FD_ZERO( &exception_fds );
+
+	while( 1 )
+	{
+		memset( buf, '\0', sizeof( buf ) );
+		/* 每次调用select 前都要重新在 read_fds 和exception_fds中设置文件描述符connfd
+		因为事件发之后，文件描述符集合将被内核修改 */
+		FD_ZERO( connfd, &read_fds );
+		FD_ZERO( connfd, &exception_fds );
+		ret = select( connfd +1, &read_fds, NULL, &exception_fds, NULL);
+		if ( ret < 0 )
+		{
+			printf("selection failure\n");
+			break;
+		}
+		/* 对于可读事件，采用普通的recv函数读取数据 */
+		if( FD_ISSET( connfd, &read_fds ) )
+		{
+			ret = recv( connfd, buf, sizeof( buf )-1, 0);
+			if( ret <=0 )
+			{
+				break;
+			}
+			printf("get %d bytes of normal data: %s\n", ret, buf );
+		}
+		/* 对于异常用事件，采用带msg_oob标专的recv读取带外数据 */
+		else if ( PD_ISSET( connfd, &exception_fds ) )
+		{
+			ret = recv( connfd, buf, sizeof( buf )-1, MSG_OOB );
+			if ( ret <=0 )
+			{
+				break;
+			}
+			printf("get %d bytes of oob data :%s\n", ret, buf );
+		}
+	}  
+	close( connfd );
+	close( listenfd );
+	return 0;
+}
