b1f94558eeb874356fdca9d147bf21971f17075d
magox <46257902@qq.com>
2017-08-10 14:18:50 +0800

    测试代码

--- IO/epoll_test.c
+++ IO/epoll_test.c
@@ -157,3 +157,49 @@ void et( epoll_event*events, int number, int epollfd, int listenfd )
 		}
 	}
 }
+
+int main( int argc, char* argv[] )
+{
+	if( argc <= 2 )
+	{
+		printf("usage: %s ip_address port_number\n", basename( argv[0] ));
+	}
+	const char* ip = argv[1];
+	int port = atoi( argv[2] );
+
+	int ret = 0;
+	struct sockaddr_in address;
+	bzero( &address, sizeof( address ) );
+	address.sin_family = AF_INET;
+	inet_pton( AF_INET, ip, &address.sin_addr );
+	address.sin_port = htons( port );
+
+	int listenfd = socket( PF_INET, SOCK_STREAM, 0 );
+	assert( listenfd >= 0 );
+
+	ret = bind( listenfd, ( struct sockaddr* )&address, sizeof( address ) );
+	assert( ret != -1 );
+	ret =  listen( listenfd, 5 );
+	assert( ret != -1 );
+
+	epoll_event events[ MAX_EVENT_NUMBER ];
+	int epollfd = epoll_create( 5 );
+	assert( ret != -1 );
+
+	addfd( epollfd, listenfd, true );
+
+	while( 1 )
+	{
+		int ret = epoll_wait( epollfd, events, MAX_EVENT_NUMBER, -1 );
+		if( ret < 0 )
+		{
+			printf("epoll failure \n");
+			break;
+		}
+		lt( events, ret, epollfd, listenfd ); //lt模式
+		// et( events, ret, epollfd, listenfd );
+	}
+	
+	close( listenfd );
+	return 0;
+}
