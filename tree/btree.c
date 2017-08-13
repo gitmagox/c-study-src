d4faa85ad4633674bc0ce5b97f2bb49354a13007
Magox ge <46257902@qq.com>
2017-08-13 20:41:35 +0800

    update

--- tree/btree.c
+++ tree/btree.c
@@ -15,22 +15,23 @@ struct b_tree_root {
 
 
 //新建一个节点
-b_tree_node _new_tree_node( Type key )
+b_tree_node _new_tree_node( Type key );
 
 //新建树,返回树的节点
-b_tree_root new_b_tree( Type key )
+b_tree_root new_b_tree( Type key );
 
 //销毁一颗树
 void destroy_b_tree( b_tree_root *root );
 
 //添加一个节点
-void add_b_tree_node( b_tree_root *root Type key )
+void add_b_tree_node( b_tree_root *root Type key );
 
 //册除一个节点
-void delete_b_tree_node( b_tree_root *root Type key )
+void delete_b_tree_node( b_tree_root *root Type key );
 
 //前序遍历
-
+void pre_order_b_tree( b_tree_node *node );
 //中序遍历
-
+void in_order_b_tree( b_tree_node *node );
 //后序遍历
+void post_order_b_tree( b_tree_node *node );
