fd79b924212710c6cf93be4aad7bce9b8f58c52d
Magox ge <46257902@qq.com>
2017-08-13 20:30:53 +0800

    void function

--- tree/btree.c
+++ tree/btree.c
@@ -14,5 +14,23 @@ struct b_tree_root {
 }
 
 
+//新建一个节点
+b_tree_node _new_tree_node( Type key )
 
+//新建树,返回树的节点
+b_tree_root new_b_tree( Type key )
 
+//销毁一颗树
+void destroy_b_tree( b_tree_root *root );
+
+//添加一个节点
+void add_b_tree_node( b_tree_root *root Type key )
+
+//册除一个节点
+void delete_b_tree_node( b_tree_root *root Type key )
+
+//前序遍历
+
+//中序遍历
+
+//后序遍历
