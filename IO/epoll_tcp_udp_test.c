df540fe31ab26758711dfc38e8ca00b1a6ca87f6
magox <46257902@qq.com>
2017-08-18 12:57:58 +0800

    epoll loop

--- IO/epoll_tcp_udp_test.c
+++ IO/epoll_tcp_udp_test.c
@@ -15,6 +15,7 @@
 #define MAX_EVENT_NUMBER 1024
 #define TCP_BUFFER_SIZE 512
 #define UDP_BUFFER_SIZE 1024
+typedef struct epoll_event epoll_event;
 
 int setnonblocking( int fd )
 {
@@ -31,4 +32,122 @@ void addfd( int epollfd, int fd )
 	event.events = EPOLLIN | EPOLLET;
 	epoll_ctl( epollfd, EPOLL_CTL_ADD, fd, &event );
 	setnonblocking( fd );
+}
+
+int main( int argc, char* argv[] )
+{
+	if( argc <=2 )
+	{
+		printf("usage: %s ip_address port_number\n",basename( argv[0] ) );
+		return 1;
+	}
+
+	//接收参数
+	const char* ip = argv[1];
+	int port = atoi( argv[2] );
+
+	int ret = 0;
+	struct sockaddr_in address;
+	bzero( &address, sizeof( address ) );
+	address.sin_port = AF_INET;
+	inet_pton( AF_INET, ip, &address.sin_addr );
+	address.sin_port = htons( port );
+
+	/*创建tcp socket*/
+	int listenfd = socket( PF_INET, SOCK_STREAM, 0 );
+	assert( listenfd >=0 );
+
+	ret = bind( listenfd, ( struct sockaddr* )&address, sizeof( address ) );
+	assert( ret !=-1 );
+
+	ret = listen( listenfd, 5 );
+	assert( ret !=-1 );
+
+	/*创建udp socket*/
+	bzero( &address, sizeof( address ) );
+	address.sin_family = AF_INET;
+	inet_pton( AF_INET, ip, &address.sin_addr );
+	address.sin_port = htons( port );
+	int udpfd = socket( PF_INET, SOCK_DGRAM, 0 );
+	assert( ret != -1 );
+
+	epoll_event events[ MAX_EVENT_NUMBER ];
+	int epollfd = epll_create( 5 );
+	assert( epollfd != -1 );
+	//注册两个可读事件
+	addfd( epollfd, listenfd );
+	addfd( epollfd, udpfd );
+
+	while( 1 )
+	{
+		int number = epoll_wait( epollfd, events, MAX_EVENT_NUMBER, -1 );
+		if ( number < 0 )
+		{
+			printf("epoll failure\n");
+			break;
+		}
+
+		int i;
+		for ( i = 0; i < number; i++ )
+		{
+			int sockfd = events[i].data.fd;
+			if( sockfd == listenfd )
+			{
+				struct sockaddr_in client_address;
+				socklen_t client_addrlength = sizeof( client_address );
+				int connfd = accept( listenfd, ( struct sockaddr* )
+								&client_address, &client_addrlength );
+				addfd( epllfd,connfd );
+			}
+			else if ( sockfd == udpfd )
+			{
+				char buf[ UDP_BUFFER_SIZE ];
+				memset( buf, '\0', UDP_BUFFER_SIZE );
+				struct sockaddr_in client_address;
+				socklen_t client_addrlength = sizeof( client_address );
+				ret = recvfrom( udpfd, buf, UDP_BUFFER_SIZE-1, 0,
+								( struct sockaddr* )&client_address, &client_addrlength  );
+				if( ret > 0 )
+				{
+					sendto( udpfd, buf, UDP_BUFFER_SIZE-1, 0,
+							( struct sockaddr* )&client_address, &client_addrlength );
+				}
+			}
+			else if ( events[i].events & EPOLLIN )
+			{
+				char buf[ TCP_BUFFER_SIZE ];
+				while( 1 )
+				{
+					memset( buf, '\0', TCP_BUFFER_SIZE );
+					ret = recv( sockfd, buf, TCP_BUFFER_SIZE-1, 0 );
+					if( ret < 0 )
+					{
+						if( ( errno == EAGAIN ) || ( errno == EWOULDBLOCK ) )
+						{
+							break;
+						}
+						close( sockfd );
+						break;
+					}
+					else if( ret == 0 )
+					{
+						close( sockfd );
+					}
+					else
+					{
+						send( sockfd, buf, ret, 0 );
+					}
+				}
+			}
+			else
+			{
+				printf("something else happened \n");
+			}
+		}
+	}
+
+	close( listenfd );
+	return 0;
+
+
 }
